9fe7c92090effa1f3b235d48ddd9fe9225c7b1e6
Michael <jtmikee@gmail.com>
2021-02-22 19:08:47 -0500

    region/change event listener

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const plant = {}
+const plantApp = {}
 const proxy = 'http://proxy.hackeryou.com';
-plant.apiUrl = 'https://trefle.io/api/v1/distributions/ONT/plants';
-plant.apiKey = 'v_NcQ2f7qlXCw9rVIw66YqesnTQnztxk1NMmt64BOuA';
+
+
+// let region = document.querySelector('#regionSelector').value;
+plantApp.apiUrl = `https://trefle.io/api/v1/distributions/${region}/plants`;
+
+plantApp.apiKey = 'v_NcQ2f7qlXCw9rVIw66YqesnTQnztxk1NMmt64BOuA';
 
 function randomOntario() {
 	return Math.floor(Math.random() * 91 + 1)
 }
 
-// get image from api 
-plant.getImages = () => {
+plantApp.getImages = () => {
 	const url = new URL(proxy);
 	url.search = new URLSearchParams({
-		reqUrl: plant.apiUrl,
-		'params[token]': plant.apiKey,
+		reqUrl: plantApp.apiUrl,
+		'params[token]': plantApp.apiKey,
 		'params[filter[establishment]': 'native',
 		'params[page]': randomOntario()
 	});
@@ -20,13 +23,27 @@ plant.getImages = () => {
 		console.log(response)
 		return response.json()
 	}).then((jsonResponse) => {
-		console.log(jsonResponse)
+		console.log(jsonResponse)	
 	});
 };
-plant.init = () => {
-	plant.getImages();
+
+
+plantApp.getRegion = () => {
+	document.querySelector('#regionSelector').addEventListener('change', (event) => {
+		const region = event.target.value;
+		console.log(region)
+		console.log('changed')
+		console.log(event)
+	})	
+}
+
+
+plantApp.init = () => {
+	plantApp.getImages();
+	plantApp.getRegion();
 };
-plant.init();
+
+plantApp.init();
 
 
 
