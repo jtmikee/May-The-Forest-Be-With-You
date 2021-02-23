c07645cd15223766fa4b3500cd91d2badcbf72bb
BeatriceDuncan <bealmduncan@gmail.com>
2021-02-23 17:43:24 -0500

    finalised JS event listner dynamic URL

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
 const plantApp = {}
 const proxy = 'http://proxy.hackeryou.com';
 
-plantApp.apiUrl = `https://trefle.io/api/v1/distributions/${region}/plants`;
-
+plantApp.apiUrl = `https://trefle.io/api/v1/distributions/ONT/plants`;
 plantApp.apiKey = 'v_NcQ2f7qlXCw9rVIw66YqesnTQnztxk1NMmt64BOuA';
 
 function randomOntario() {
 	return Math.floor(Math.random() * 91 + 1)
 }
 
+// prevent default bahaviour
+// filter image requirements - only display if there IS an image url (like fashion codealong we did)
+
 plantApp.getImages = () => {
 	const url = new URL(proxy);
 	url.search = new URLSearchParams({
 		reqUrl: plantApp.apiUrl,
 		'params[token]': plantApp.apiKey,
-		'params[filter[establishment]': 'native',
-		'params[page]': randomOntario()
+		'params[filter[establishment]]': 'native',
+		'params[page]': '10'
 	});
 	fetch(url).then((response) => {
 		console.log(response)
 		return response.json()
 	}).then((jsonResponse) => {
 		console.log(jsonResponse)	
+	}).catch(error => {
+		console.log(error);
 	});
 };
 
-plantApp.getRegion = () => {
+function getRegion(event) {
+	plantApp.apiUrl = `https://trefle.io/api/v1/distributions/${event.target.value}/plants`;
+	plantApp.getImages();
+	console.log(plantApp.apiUrl)
+}
+
+plantApp.createEventListener = () => {
 	document.querySelector('#regionSelector').addEventListener('change', (event) => {
-		const region = event.target.value;
+		getRegion(event);
 	})	
 }
 
-
 plantApp.init = () => {
-	plantApp.getImages();
-	plantApp.getRegion();
+	plantApp.createEventListener();
 };
 
 plantApp.init();
-
-
-
