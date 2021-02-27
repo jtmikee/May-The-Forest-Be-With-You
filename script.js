9d20cb922e598498a607d31164a6a7bd1b75f158
BeatriceDuncan <bealmduncan@gmail.com>
2021-02-27 17:46:13 -0500

    added mixins & mediaqueries

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ function randomPage() {
 	return Math.floor(Math.random() * 25 + 1)
 }
 
-// prevent default bahaviour
-// filter image requirements - only display if there IS an image url (like fashion codealong we did)
-
 plantApp.getPlants = () => {
 	const url = new URL(proxy);
 	url.search = new URLSearchParams({
@@ -18,7 +15,6 @@ plantApp.getPlants = () => {
 		'params[token]': plantApp.apiKey,
 		'params[filter[establishment]]': 'native',
 		'params[filter_not[ATTRIBUTE]]': 'null',
-		// 'params[]' // potentially remove https
 		'params[page]': '1'
 	});
 	fetch(url).then((response) => {
@@ -45,6 +41,7 @@ plantApp.displayCards = (plantArray) => {
 
 	const treeCard = document.querySelector('#cardHolder');
 	treeCard.innerHTML = '';
+
 	plantArray.forEach((plant) => {
 
 		const cardContainer = document.createElement('div');
@@ -55,70 +52,29 @@ plantApp.displayCards = (plantArray) => {
 
 		const plantImage = document.createElement('img');
 
+		plant.image_url = plant.image_url.replace('https://bs.floristic.org', 'http://bs.floristic.org');
 
-		if (plant.image_url.src == onload) {
-			plantImage.src = plant.image_url;
-		}
-
-		plantImage.alt = plant.common_name;
-
-		const plantTitle = document.createElement('h2');
-		plantTitle.innerText = plant.common_name;
-
-		imageContainer.appendChild(plantImage);
-		cardContainer.append(imageContainer, plantTitle);
-
-		treeCard.appendChild(cardContainer);
-
-
-
-
-	})
-}
-
-plantApp.displayCards = (plantArray) => {
-
-	const treeCard = document.querySelector('#cardHolder');
-	treeCard.innerHTML = '';
-	plantArray.forEach((plant) => {
-
-		const cardContainer = document.createElement('div');
-		cardContainer.classList.add('cardContainer');
-
-		const imageContainer = document.createElement('div');
-		imageContainer.classList.add('imageContainer');
-
-		const plantImage = document.createElement('img');
 		plantImage.src = plant.image_url;
 		plantImage.alt = plant.common_name;
 
 		const plantTitle = document.createElement('h2');
 		plantTitle.innerText = plant.common_name;
 
-
 		imageContainer.appendChild(plantImage);
 		cardContainer.append(imageContainer, plantTitle);
 
 		treeCard.appendChild(cardContainer);
 
-		console.log(plantArray)
-
-		//TESTING
-		// removeCharacterS = (httpsURL, positionOfS) => {
-		// 	part1 = httpsURL.substring(0, positionOfS);
-		// 	part2 = httpsURL.substring(positionOfS + 1, httpsURL.length);
-		// }
-
-		// const imgUrl = plant.image_url;
-		// console.log(removeCharacterS(imgUrl, 6))
-
 	})
 }
 
+plantApp.reload = () => {
+	document.querySelector('form').reset();
+}
+
 plantApp.init = () => {
-	plantApp.createEventListener();
+	plantApp.createEventListener()
+	plantApp.reload();
 };
 
 plantApp.init();
-
-
