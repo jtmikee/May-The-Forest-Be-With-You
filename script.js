d48353b9f6d7b9ebbe8c81dc56ed3973bdedb22e
BeatriceDuncan <bealmduncan@gmail.com>
2021-02-28 16:38:02 -0500

    troubleshot foristic 'bug'/update

--- script.js
+++ script.js
@@ -15,15 +15,16 @@ plantApp.getPlants = () => {
 		'params[token]': plantApp.apiKey,
 		'params[filter[establishment]]': 'native',
 		'params[filter_not[ATTRIBUTE]]': 'null',
+		'params[filter_not[common_name]]': 'null',
 		'params[page]': randomPage()
 	});
 	fetch(url).then((response) => {
-		return response.json()
+		return response.json();
 	}).then((jsonResponse) => {
 		plantApp.displayCards(jsonResponse.data)
 	}).catch(error => {
 		console.log(error);
-	});
+	})
 };
 
 plantApp.getRegion = (event) => {
@@ -54,45 +55,60 @@ plantApp.displayCards = (plantArray) => {
 		plantImage.classList.add('defaultImage');
 
 		if (plant.image_url) {
-			plant.image_url = plant.image_url.replace('https://bs.floristic.org', 'http://bs.floristic.org');
+			// plant.image_url = plant.image_url.replace('https://bs.floristic.org', 'http://bs.floristic.org');
 
-			plantImage.src = plant.image_url;
-			plantImage.alt = plant.common_name;
+			//Feb 28 (the day before due-date)
+			// Hello project marking person! 
+			//After completing our beautiful app last night, this morning we woke to the realisation that one of the websites our API was calling on to collect images (floristic.org) stopped working for a handfull of images. We suspect and update or something like that. Previously images from this source had been displaying an error, and we were proud to work out the above line of code (line 58) that enabled us to access the images. This took us a long time to work out and we were sad to see all that problem-solving go to waste. But we are pleased with our workaround! We did leave line 58 in there though incase whatever is happening on floristic's end gets sorted and you would like to see it in full swing. You'll need to remove lines 65-67 and line 110.
+			//Thanks for your time!
+			//Michael and Bea.
 
-			const plantTitle = document.createElement('h2');
-			plantTitle.innerText = plant.common_name;
+			if (plant.image_url.includes('floristic')) {
+				return ""
+				} else {
+			
+				plantImage.src = plant.image_url;
+				plantImage.alt = plant.common_name;
 
-			imageContainer.appendChild(plantImage);
+				const plantTitle = document.createElement('h2');
+				plantTitle.innerText = plant.common_name;
 
-			const plantFamilyHolder = document.createElement('h2');
-			plantFamilyHolder.innerText = ''
+				imageContainer.appendChild(plantImage);
 
-			const plantCommonHolder = document.createElement('h3');
-			plantCommonHolder.innerText = ''
+				const plantFamilyHolder = document.createElement('h2');
+				plantFamilyHolder.innerText = ''
 
-			cardContainer.prepend(plantFamilyHolder)
-			cardContainer.append(imageContainer, plantTitle, plantCommonHolder);
-			treeCard.appendChild(cardContainer);
+				const plantCommonHolder = document.createElement('h3');
+				plantCommonHolder.innerText = ''
 
-			plantImage.addEventListener('click', () => {
+				cardContainer.prepend(plantFamilyHolder)
+				cardContainer.append(imageContainer, plantTitle, plantCommonHolder);
 
-				imageContainer.classList.remove('imageContainer')
-				imageContainer.classList.add('plantInfoContainer')
-				plantImage.classList.add('plantInfoImage')
-	
-				const plantSciName = document.createElement('h2');
-				plantSciName.innerText = `${plant.scientific_name}`
-				plantSciName.classList.add('plantSciName');
-				
-				const plantFamily = document.createElement('h2');
-				plantFamily.innerText = `Family: ${plant.family_common_name}`
+				treeCard.appendChild(cardContainer);
 
-				plantTitle.innerText = plantSciName.innerText;
+				console.log(plantArray)
 
-				plantFamilyHolder.innerText = `Scientific Name:`
+				plantImage.addEventListener('click', () => {
 
-				plantCommonHolder.innerText = plant.common_name
-			})
+					imageContainer.classList.remove('imageContainer')
+					imageContainer.classList.add('plantInfoContainer')
+					plantImage.classList.add('plantInfoImage')
+		
+					const plantSciName = document.createElement('h2');
+					plantSciName.innerText = `${plant.scientific_name}`
+					plantSciName.classList.add('plantSciName');
+					
+					const plantFamily = document.createElement('h2');
+					plantFamily.innerText = `Family: ${plant.family_common_name}`
+
+					plantTitle.innerText = plantSciName.innerText;
+
+					plantFamilyHolder.innerText = `Scientific Name:`
+
+					plantCommonHolder.innerText = plant.common_name
+
+				})
+			} 
 		}
 	})
 }
